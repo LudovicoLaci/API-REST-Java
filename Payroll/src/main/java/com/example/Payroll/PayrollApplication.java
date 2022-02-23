290d4e345ec35490bef7f98a1bfd18cfcd5cbe74
can-er <2broglie@gmail.com>
2022-02-23 18:03:29 +0100

    adding new files

--- Payroll/src/main/java/com/example/Payroll/PayrollApplication.java
+++ Payroll/src/main/java/com/example/Payroll/PayrollApplication.java
@@ -1,5 +1,6 @@
 package com.example.Payroll;
 
+//package org.springframework.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
